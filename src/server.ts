83a727a3c9aa89277b662bb5b2a266a468585403
julioa-adl <julioader@gmail.com>
2023-09-27 09:45:33 -0300

    implementando o atlas

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,4 @@ import connectToDatabase from './config/Connection';
 
 const PORT = process.env.PORT || 3001;
 connectToDatabase()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
-  })
-  .catch((error) => {
-    console.log('Connection with database generated an error:\r\n');
-    console.error(error);
-    console.log('\r\nServer initialization cancelled');
-    process.exit(0);
-  });
+app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
