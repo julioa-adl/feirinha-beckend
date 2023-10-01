6c390413290c5a92f4f498df066db722693c3bb7
julioa-adl <julioader@gmail.com>
2023-10-01 14:30:04 -0300

    fix: create corsOption

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,12 @@ import app from './app';
 import connectToDatabase from './config/Connection';
 import cors from 'cors';
 
+const corsOptions ={
+  origin:'http://localhost:5173', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
