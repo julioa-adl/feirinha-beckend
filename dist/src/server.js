2e9a7e18ff37e31a5d3b49c3b401e4e0ce6f2071
julioa-adl <julioader@gmail.com>
2023-10-01 14:30:45 -0300

    fix: build ts corsOptions

--- dist/src/server.js
+++ dist/src/server.js
@@ -8,6 +8,12 @@ require("dotenv/config");
 const app_1 = __importDefault(require("./app"));
 const Connection_1 = __importDefault(require("./config/Connection"));
 const cors_1 = __importDefault(require("cors"));
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app_1.default.use((0, cors_1.default)(corsOptions));
 app_1.default.use((0, cors_1.default)());
 const PORT = process.env.PORT || 3001;
 (0, Connection_1.default)();
