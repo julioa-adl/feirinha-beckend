1c47f0479288e914768a6e045e291ab4db8adb72
julioa-adl <julioader@gmail.com>
2023-10-01 14:24:58 -0300

    feat: install cors

--- dist/src/server.js
+++ dist/src/server.js
@@ -7,6 +7,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const app_1 = __importDefault(require("./app"));
 const Connection_1 = __importDefault(require("./config/Connection"));
+const cors_1 = __importDefault(require("cors"));
+app_1.default.use((0, cors_1.default)());
 const PORT = process.env.PORT || 3001;
 (0, Connection_1.default)();
 app_1.default.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@
 import 'dotenv/config';
 import app from './app';
 import connectToDatabase from './config/Connection';
+import cors from 'cors';
 
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 connectToDatabase()
