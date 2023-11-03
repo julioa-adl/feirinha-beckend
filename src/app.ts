e458d38d01fb95dbfcb95cf9352c5892448aedb2
julioa-adl <julioader@gmail.com>
2023-11-03 19:05:32 -0300

    feat

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import defaultRouter from './routes/Default.Routes';
 import cors from 'cors';
 
 const corsOptions ={
-  origin:'http://localhost:5174', 
+  origin:'*', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
