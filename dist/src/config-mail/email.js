662ae6e6272867d19abf76aa403a935ed563ec98
julioa-adl <julioafmesquita@gmail.com>
2024-04-02 17:59:52 -0300

    teeeeste email

--- dist/src/config-mail/email.js
+++ dist/src/config-mail/email.js
@@ -17,15 +17,17 @@ async function sendVerificationCode(toEmail, verificationCode) {
         to: toEmail,
         subject: "Your verification code",
         html: `
-        <div style="padding:20px 0px; background-color: #e1e1ef; display: flex; justify-content: center; align-items: center; height: 100%; width: 100%;">
-            <div style="background-color: #ffffff; padding:8px; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);">
-                <h2>Olá! Seja bem vindo ao Feirinha da Gente <3</h2>
-                <p>Este é seu código de verificação de Email <br/>
-                copie e cole na página de cadastro para realizar a validação! <br/>
-                Este código irá expirar em 10 min!</p>
-                <strong>${verificationCode}</strong>
-                <p>Em caso de dúvidas, basta responder este Email.
-                Obrigado!</p>
+        <div style="margin: 0; padding: 0; background-color: #e1e1ef; height: 100vh;">
+            <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
+                <div style="background-color: #ffffff; padding: 20px; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);">
+                    <h2>Olá! Seja bem-vindo ao Feirinha da Gente <3</h2>
+                    <p>Este é seu código de verificação de Email <br>
+                    copie e cole na página de cadastro para realizar a validação! <br>
+                    Este código irá expirar em 10 min!</p>
+                    <strong>${verificationCode}</strong>
+                    <p>Em caso de dúvidas, basta responder este Email.
+                    Obrigado!</p>
+                </div>
             </div>
         </div>
         `
