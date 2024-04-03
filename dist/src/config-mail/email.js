210a4ba51362e6372563aebb680f0e11641eee34
julioa-adl <julioafmesquita@gmail.com>
2024-04-02 21:55:09 -0300

    teste ee

--- dist/src/config-mail/email.js
+++ dist/src/config-mail/email.js
@@ -18,14 +18,26 @@ async function sendVerificationCode(toEmail, verificationCode) {
         subject: "Your verification code",
         html: `
             <div style="display: flex; justify-content: center; align-items: center; height: 100%; padding: 20px; background-color: #e1e1ef;">
-                <div style="margin: auto; background-color: #ffffff; padding: 8px; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);">
-                    <h2>Olá! Seja bem-vindo ao Feirinha da Gente <3</h2>
-                    <p>Este é seu código de verificação de Email <br>
-                    copie e cole na página de cadastro para realizar a validação! <br>
-                    Este código irá expirar em 10 min!</p>
-                    <strong>${verificationCode}</strong>
-                    <p>Em caso de dúvidas, basta responder este Email.
-                    Obrigado!</p>
+                <div style="margin: auto; background-color: #ffffff; padding: 16px 8px; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);">
+                    <h1>E aí!</h1>
+                    <p>Seja bem-vindo ao Feirinha da Gente <3,<br> é um grande prazer ter você por aqui.</p>
+                    <br>
+                    <p>Para prosseguir com seu cadastro, por favor, <br>
+                    utilize o seguinte código de cofirmação: </p>
+                    <h1><strong>${verificationCode}</strong></h1>
+                    <p>Por favor, insira este código no campo apropriado<br>
+                     no nosso site para continuar o processo de cadastro<br>
+                     de sua conta. Este código é válido por 2 dias e deve ser<br>
+                     usado imediatamente para garantir a segurança da sua conta.
+                     <br>
+                     Se você não solicitou este código, ignore este e-mail.<br>
+                     Nenhuma conta será feita em sua nome. Para qualquer dúvida<br>
+                     ou necessidade de assistência adicional, entre em contato conosco<br>
+                     pelo e-mail feirinhadagente00@gmail.com.
+                     <br>
+                     Um abraço,<br>
+                     Equipe Feirinha da Gente.
+                     </p>
                 </div>
             </div>
         `
