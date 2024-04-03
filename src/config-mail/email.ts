36dc0151a23884deb92541ab6d29e24f24234bf9
julioa-adl <julioafmesquita@gmail.com>
2024-04-02 22:02:00 -0300

    style="font-size: 16px;"

--- src/config-mail/email.ts
+++ src/config-mail/email.ts
@@ -19,12 +19,12 @@ export async function sendVerificationCode(toEmail: string, verificationCode: st
             <div style="display: flex; justify-content: center; align-items: center; height: 100%; padding: 20px; background-color: #e1e1ef;">
                 <div style="margin: auto; background-color: #ffffff; padding: 25px; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);">
                     <h2>Opa, cheguei!</h2>
-                    <p>Seja bem-vindo ao Feirinha da Gente <3,<br> é um grande prazer ter você por aqui.</p>
+                    <p style="font-size: 16px;">Seja bem-vindo ao Feirinha da Gente <3,<br> é um grande prazer ter você por aqui.</p>
                     <br>
                     <p style="font-size: 16px;">Para prosseguir com seu cadastro, por favor, <br>
                     utilize o seguinte código de cofirmação: </p>
                     <h1><strong>${verificationCode}</strong></h1>
-                    <p>Por favor, insira este código no campo apropriado<br>
+                    <p style="font-size: 16px;">Por favor, insira este código no campo apropriado<br>
                      no nosso site para continuar o processo de cadastro<br>
                      de sua conta. Este código é válido por 2 dias e deve ser<br>
                      usado imediatamente para garantir a segurança da sua conta.
